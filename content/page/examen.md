626af7b1b30e6cac8948af444d102da82a9d888f
mmarnun <123729868+mmarnun@users.noreply.github.com>
2023-11-16 09:11:17 +0100

    Commit

---
+++ content/page/examen.md
@@ -0,0 +1,11 @@
+---
+title: "Prueba: Introduccion a la implantacion de aplicaciones web"
+date: 2023-11-16T08:58:38+01:00
+tags: []
+featured_image: ""
+description: ""
+---
+
+## Manuel Alejandro Martin Nunez
+
+![Ejemplo de imagen](/page/images/alex.png)
--- script.sh
+++ script.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 
-# Define variables
-GIT_REPO="https://github.com/mmarnun/hugo_web"
-RENDER_SERVICE_NAME="hugo site alex"
-RENDER_TOKEN="rnd_cY6uu8PYbJkpdYYwE7STInFosRk1"
+git add .
 
-# Configure Render CLI
-echo "Configurando Render CLI..."
-render account:token $RENDER_TOKEN
-render service:select $RENDER_SERVICE_NAME
+git commit -m "Commit"
 
-# Pull the latest changes from the Git repository
-echo "Actualizando el repositorio Git..."
-git pull origin main
+git push origin master
 
-# Generate the Hugo site
-echo "Generando el sitio Hugo..."
-hugo server
-
-# Deploy to Render.com
-echo "Desplegando en Render.com..."
-render up
-
-echo "¡Despliegue completado!"
+echo "Cambios guardados y desplegados en Render exitosamente."
